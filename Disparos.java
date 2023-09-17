7d3e3fb28b13f92361e61ac3ca839d1e78f52a07
Trangulos <jesicaa.romerom@konradlorenz.edu.co>
2023-09-16 20:07:15 -0500

    Se crea el tamaño de la matriz que es de 7x7 con asteriscos y se crea el barco1 donde se colocan dos barcos que ocupan una casilla aleatoriamente  en la matriz.

--- Disparos.java
+++ Disparos.java
@@ -1,2 +1,43 @@
+import java.util.Random;
+import java.util.Scanner;
 public class Disparos {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int TAMANO = 7;
+        char[][] matriz = inicializarMatriz(TAMANO);
+        barco1(matriz,TAMANO);
+        imprimirMatriz(matriz);
+    }
+    private static char[][] inicializarMatriz(int TAMANO) {
+        char[][] matriz = new char[TAMANO][TAMANO];
+        for (int i = 0; i < TAMANO; i++) {
+            for (int j = 0; j < TAMANO; j++) {
+                matriz[i][j] = '*';
+            }
+        }
+        return matriz;
+    }
+    private static void barco1(char[][] matriz, int TAMANO) {
+        Random random = new Random();
+        for (int i = 0; i < 2; i++) {
+            int fila = random.nextInt(TAMANO);
+            int columna = random.nextInt(TAMANO);
+
+            while (matriz[fila][columna] != '*') {
+                fila = random.nextInt(TAMANO);
+                columna = random.nextInt(TAMANO);
+            }
+
+            char barco = (char) ('D' + i); // 'A', 'B', 'C', ...
+            matriz[fila][columna] = barco;
+        }    }
+    private static void imprimirMatriz(char[][] matriz) {
+        System.out.println("Matriz con barcos:");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
